a9c97cb7147b4b688765cc2450a82f70761e946d
Jpazda <111332675+Jpazda@users.noreply.github.com>
2023-08-01 00:30:44 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const subjects = ["Dog", "Brother", "Cat", "pet Tiger", "Teacher", "Boss"];
+
+  const verbs = ["ate", "destroyed", "lost", "threw away", "forgot"];
+
+  const possessions = ["homework", "car", "shoes", "shirt", "computer"];
+
+  const times = [
+    "yesterday",
+    "last week",
+    "this morning",
+    "last night",
+    "on my birthday",
+    "on Christmas"
+  ];
+
+  const randomSubjectsIndex = Math.floor(Math.random() * subjects.length);
+  const randomVerbsIndex = Math.floor(Math.random() * verbs.length);
+  const randomPossessionsIndex = Math.floor(Math.random() * possessions.length);
+  const randomTimesIndex = Math.floor(Math.random() * times.length);
+
+  const pageHeading = document.querySelector("#header");
+  console.log(pageHeading);
+
+  pageHeading.textContent = `My ${subjects[randomSubjectsIndex]} ${verbs[randomVerbsIndex]} my ${possessions[randomPossessionsIndex]} ${times[randomTimesIndex]}`;
 };
